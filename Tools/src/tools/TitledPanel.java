3f7e5856e90eaaf994ec528eaf3e240b361abbd7
phil <phil@Thinky>
2015-08-06 19:29:28 +0200

    .

--- Tools/src/tools/TitledPanel.java
+++ Tools/src/tools/TitledPanel.java
@@ -3,15 +3,16 @@ package tools;
 import java.awt.Component;
 import java.awt.GridLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
 
 public class TitledPanel extends JPanel
 {
 	public TitledPanel(String title, Component comp)
 	{
-		setLayout(new GridLayout(1, 1));
-		setBorder(new TitledBorder(title));
+		setLayout(new GridLayout(-1, 1));
+		//setBorder(new TitledBorder(title));
+		add(new JLabel(title));
 		add(comp);
 	}
 }
--- Tools/src/tools/swing/TitledJFileChooser.java
+++ Tools/src/tools/swing/TitledJFileChooser.java
@@ -2,6 +2,7 @@ package tools.swing;
 
 import java.awt.Component;
 import java.awt.HeadlessException;
+import java.io.File;
 
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
@@ -34,5 +35,18 @@ public class TitledJFileChooser extends JFileChooser
 			dialog.setTitle(dialogTitle);
 		}
 		return dialog;
+	}
+	
+	
+	public static File requestFolderName(String title, String defaultFolder, Component parent)
+	{
+		TitledJFileChooser fc = new TitledJFileChooser(defaultFolder);
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);	
+		//fc.setCurrentDirectory(new File(defaultFolder));
+		fc.setDialogTitle(title);
+		fc.setApproveButtonText("Set");
+		fc.showOpenDialog(parent);
+		File sf = fc.getSelectedFile();
+		return sf;
 	}
 }
