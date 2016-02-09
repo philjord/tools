373d543a419a0bbcf5a3b2b1fdd8b74a383bd797
phil <phil@Thinky>
2016-02-09 10:56:05 +0100

    AWT gear from projects moved around

--- Tools/src/tools/io/FileCopy.java
+++ Tools/src/tools/io/FileCopy.java
@@ -2,7 +2,10 @@ package tools.io;
 
 import java.io.Closeable;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
@@ -13,6 +16,41 @@ import com.sun.istack.internal.Nullable;
 
 public class FileCopy
 {
+	public static void copyInputStreamToFile(InputStream in, File file)
+	{
+		OutputStream out = null;
+		try
+		{
+			out = new FileOutputStream(file);
+			byte[] buf = new byte[1024 * 1024];
+			int len;
+			while ((len = in.read(buf)) > 0)
+			{
+				out.write(buf, 0, len);
+			}
+			out.close();
+			in.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (out != null)
+			{
+				try
+				{
+					out.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
 	// Copy one file to another using NIO
 	public static void doCopy(final File source, final File destination) throws IOException
 	{
